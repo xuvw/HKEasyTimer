0d23a12a9adf9ca1de9999c42190c39df3cb57d8
ke he <smileshitou@hotmail.com>
2016-07-30 00:47:29 +0800

    Initial Commit

---
+++ HKEasyTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HKEasyTimer
+//
+//  Created by heke on 16/7/30.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
